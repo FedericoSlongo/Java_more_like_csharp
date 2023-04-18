0209b391211d49bfd9977ff60c59500506bd15f3
Federico Slongo <73640124+FedericoSlongo@users.noreply.github.com>
2023-04-18 20:02:49 +0100

    Files to manage files
    Rn only reads a/all lines of a file

---
+++ files.java
@@ -0,0 +1,38 @@
+/*
+ * Library created by Federico Slongo.
+ * https://github.com/FedericoSlongo
+ *
+ * This .java is part of the https://github.com/FedericoSlongo/Java_more_like_csharp repo
+ */
+
+package [package_name];
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class files {
+    private BufferedReader fr;
+    private String line;
+
+    public files(String file_name) throws FileNotFoundException {
+        fr = new BufferedReader(new FileReader(file_name));
+        line = "";
+    }
+
+    public String ReadLine() throws IOException {
+        line=fr.readLine();
+        if(line != null)
+            return fr.readLine();
+        else
+            return null;
+    }
+
+    public String ReadAllLine() throws IOException {
+        String Everything = "";
+        while((line=fr.readLine())!=null)
+            Everything += line;
+        return Everything;
+    }
+}
